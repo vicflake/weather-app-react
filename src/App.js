9abc8f7c52d656d01c9f5c4072b476d5e7412932
Victoria Moreno <vmlopez4@gmail.com>
2021-05-16 15:31:53 +0100

    Footer with github link added.

--- src/App.js
+++ src/App.js
@@ -11,16 +11,22 @@ import Time from "./Time";
 
 function App() {
   return (
-    <div className="App">
-      <Form />
-      <Time time="Sun 10:00" />
-      <WeatherBox
-        city="California"
-        description="Sunny"
-        icon="https://png.pngtree.com/png-vector/20190629/ourmid/pngtree-sun-icon-design-png-image_1518941.jpg"
-      />
-      <Temperature temp={16} />
-      <WeatherInfo humidity="80" wind="15" precipitation="20" />
+    <div>
+      <div className="App">
+        <Form />
+        <Time time="Sun 10:00" />
+        <WeatherBox
+          city="California"
+          description="Sunny"
+          icon="https://png.pngtree.com/png-vector/20190629/ourmid/pngtree-sun-icon-design-png-image_1518941.jpg"
+        />
+        <Temperature temp={16} />
+        <WeatherInfo humidity="80" wind="15" precipitation="20" />
+      </div>
+      <footer>
+        <a href="https://github.com/vicflake/weather-app-react">Open-source</a>
+        <span> by Victoria Moreno</span> 
+      </footer>
     </div>
   );
 }
