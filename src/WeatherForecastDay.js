320d2180729c7aaf057d9587d7be7753b7332216
Victoria Moreno <vmlopez4@gmail.com>
2021-06-13 18:35:15 +0100

    5day forecast API working.

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -24,7 +24,7 @@ export default function WeatherForecastDay(props){
             <div className="WeatherForecast-day">
                     {day()}
                 </div>
-                    <WeatherIcon code={props.data.weather[0].icon} size={25}/>
+                    <WeatherIcon code={props.data.weather[0].icon} size={20}/>
                     <div className="WeatherForecast-temperatures">
                         <span className="WeatherForecast-max">{max()}</span>
                         <span className="WeatherForecast-min">{min()}</span>
